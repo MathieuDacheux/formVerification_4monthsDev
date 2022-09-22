d144f8bd942c59d67119dece274042cc5bb89369
MathieuDacheux <mathieu.dacheux@icloud.com>
2022-09-22 11:02:23 +0200

    Verification et validation des 5 premiers champs

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     $countries = array ("Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe");
+    $degree = array ("Sans", "Baccalauréat", "Baccalauréat +2", "Baccalauréat +3", "Baccalauréat +5", "Doctorat");
     
     include './public/php/regex.php';
     
@@ -7,24 +8,48 @@
         include './public/php/data.php';
         foreach ($inputs as $key => $input) {
             // Nettoyage
-            $input['value'] .= trim(filter_input(INPUT_POST, $input['name'], $input['filter']));
+            $input['value'] .= trim(filter_input(INPUT_POST, $key, $input['filter']));
             // Validation
-            if (empty($input['value'])) {
-                $input['error'] .= 'Ce champ est obligatoire';
-                $inputs[$key] = $input;
-            } else {
-                $isOk = filter_var($input['value'], FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => '/'.$input['regex'].'/')));
-                if ($isOk == false) {
+            if ($key == 'birthCountry') {
+                if (empty($input['value'])) {
+                    $input['error'] .= 'Ce champ est obligatoire';
+                } else if (in_array($input['value'], $countries) == false) {
                     $input['error'] .= 'La donnée n\'est pas conforme';
-                    $inputs[$key] = $input;
                 } else {
-                    $input['value'] = $isOk;
-                    $inputs[$key] = $input;
+                    if (!$isOk) {
+                        $isOk = filter_var($input['value'], FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => '/'.$input['regex'].'/')));
+                    } else {
+                        $input['value'] = $isOk;
+                    }
+                }
+            } else if ($key == 'graduate') {
+                if (empty($input['value'])) {
+                    $input['error'] .= 'Ce champ est obligatoire';
+                } else if (in_array($input['value'], $degree) == false) {
+                    $input['error'] .= 'La donnée n\'est pas conforme';
+                } else {
+                    if (!$isOk) {
+                        $isOk = filter_var($input['value'], FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => '/'.$input['regex'].'/')));
+                    } else {
+                        $input['value'] = $isOk;
+                    }
+                }
+            } else {
+                if (empty($input['value'])) {
+                    $input['error'] .= 'Ce champ est obligatoire';
+                } else {
+                    $isOk = filter_var($input['value'], FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => '/'.$input['regex'].'/')));
+                    if (!$isOk) {
+                        $input['error'] .= 'La donnée n\'est pas conforme';
+                    } else {
+                        $input['value'] = $isOk;
+                    }
                 }
             }
-        }
-        var_dump($inputs);
+        $inputs[$key] = $input;
     }
+    var_dump($inputs);
+}
 ?>
 
 <!DOCTYPE html>
@@ -56,9 +81,7 @@
             <!-- Input nom et prénom -->
             <div class="containerInput">
                 <input value="<?= $inputs['lastname']['value'] ?? '' ?>" class="input" type="text" name="lastname" id="lastname" placeholder="Nom" pattern="<?= REGEX_NO_NUMBER ?>"required>
-                <p class="error"><?= $inputs['lastname']['error'] ?? '' ?></p>
                 <input class="input" type="text" name="firstname" id="firstname" placeholder="Prénom" pattern="<?= REGEX_NO_NUMBER ?>" required>
-                <p class="error"><?= $inputs['firstname']['error'] ?? '' ?></p>
             </div>
 
             <!-- Input date de naissance -->
@@ -74,7 +97,7 @@
                         }
                     ?>
                 </select>
-                <input class="input" type="number" name="zipCode" id="zipCode" placeholder="Code postal" pattern="[0-9]{5,}">
+                <input class="input" type="number" name="zipCode" id="zipCode" placeholder="Code postal" pattern="[0-9]{5}">
             </div>
 
             <!-- Input mail -->
@@ -95,12 +118,12 @@
 
             <!-- Input niveau d'étude -->
             <select name="graduate" id="graduate" required>
-                <option value="0">Sans</option>
-                <option value="1" selected>Baccalauréat</option>
-                <option value="2">Baccalauréat +2</option>
-                <option value="3">Baccalauréat +3</option>
-                <option value="4">Baccalauréat +5</option>
-                <option value="5">Doctorat</option>
+                <option>Sans</option>
+                <option selected>Baccalauréat</option>
+                <option>Baccalauréat +2</option>
+                <option>Baccalauréat +3</option>
+                <option>Baccalauréat +5</option>
+                <option>Doctorat</option>
             </select>
 
             <!-- Input compétence en programmation -->
--- public/php/data.php
+++ public/php/data.php
@@ -1,21 +1,48 @@
 <?php
     $inputs = [
+        'graduate' => [
+            'filter' => FILTER_SANITIZE_SPECIAL_CHARS,
+            'regex' => REGEX_NO_NUMBER,
+            'error' => '',
+            'value' => '',
+        ],
+        'linkedinAccount' => [
+            'filter' => FILTER_SANITIZE_URL,
+            'regex' => REGEX_URL_LINKEDIN,
+            'error' => '',
+            'value' => '',
+        ],
+        'email' => [
+            'filter' => FILTER_SANITIZE_EMAIL,
+            'regex' => REGEX_EMAIL,
+            'error' => '',
+            'value' => '',
+        ],
+        'zipCode' => [
+            'filter' => FILTER_SANITIZE_SPECIAL_CHARS,
+            'regex' => REGEX_FIVE_NUMBERS,
+            'error' => '',
+            'value' => '',
+        ],
+        'birthCountry' => [
+            'filter' => FILTER_SANITIZE_SPECIAL_CHARS,
+            'regex' => REGEX_NO_NUMBER,
+            'error' => '',
+            'value' => '',
+        ],
         'birthday' => [
-            'name' => 'birthday',
             'filter' => FILTER_SANITIZE_SPECIAL_CHARS,
             'regex' => REGEX_BIRTHDAY,
             'error' => '',
             'value' => '',
         ],
         'firstname' => [
-            'name' => 'firstname',
             'filter' => FILTER_SANITIZE_SPECIAL_CHARS,
             'regex' => REGEX_NO_NUMBER,
             'error' => '',
             'value' => '',
         ],
         'lastname' => [
-            'name' => 'lastname',
             'filter' => FILTER_SANITIZE_SPECIAL_CHARS,
             'regex' => REGEX_NO_NUMBER,
             'error' => '',
